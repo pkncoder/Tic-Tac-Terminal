943232eb9a1eae67fafdb7c80809a867559f81d3
pkncoder <128310642+pkncoder@users.noreply.github.com>
2023-05-03 02:16:18 +0000

    Welcome to Tic-Tac-Toe v1.0. This is a base game.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from TicTacFuncs import *
+from TicTacFuncs import playerflip, printboard, testwin
 
 board = ['-', '-', '-',
          '-', '-', '-',
@@ -13,11 +13,17 @@ printboard(board)
 while GameOn != False:
     if player == 'P1':
         P1Choice = int(input('Pick from 1-9, Player 1: '))
-        board[P1Choice - 1] = 'X'
-        player = playerflip(player)
+        if board[P1Choice - 1] != '-':
+            print("This square is already taken!")
+        else:
+            board[P1Choice - 1] = 'X'
+            player = playerflip(player)
     elif player == 'P2':
         P2Choice = int(input('Pick from 1-9, Player 2: '))
-        board[P2Choice - 1] = 'O'
-        player = playerflip(player)
+        if board[P2Choice - 1] != '-':
+            print("This square is already taken")
+        else:
+            board[P2Choice - 1] = 'O'
+            player = playerflip(player)
     printboard(board)
     GameOn = testwin(board, GameOn)
