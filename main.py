2339c7b016cf853770e940117e04757d84724ec6
pkncoder <alex.h.preston@gmail.com>
2023-06-09 20:18:51 -0400

    Created a number of round but not a who win by avg

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from TicTacFuncs import printTable, printTableNums, test, startturn, turnflip
 
-slots = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
 errors = 0
 
-win = False
 ster1 = True
 ster2 = True
-us1inperr = True
-
-turn = startturn()
+rounderr = True
 
 usr1n = input("What is your name, player one: ")
 
@@ -45,78 +41,110 @@ while ster2 == True:
         print("Player two has been created for this game, let's go!")
         ster2 = False
 
+while rounderr == True:
 
-while win == False:
-    if turn == 'us1':
-        printTable(slots)
-        printTableNums()
+    try:
+        rounds = int(input("How many rounds would you like: "))
+        break
 
-        while True:
-            try:
-                us1ch = int(input(f"What is your choice {usr1n}? Use numbers 1 to 9 to take your pick. Numbers are shown on the second table: "))
-                break
-
-            except ValueError:
-                print("This isn't a number, please try again")
+    except ValueError:
+        print("That is not a number, or somthing else went wrong. Please try again.")
+        errors += 1
 
 
-        if us1ch >= 10:
-            print("Sorry, that's out of the range of the table, please try again.")
-            i = 1
-            errors += 1
+for x in range(rounds):
 
-        else:
-            if slots[us1ch - 1] == '-':
-                slots[us1ch - 1] = usr1Cch
-                i = 0
+    win = False
+    turn = startturn()
 
-            else:
-                print("Sorry, that tile is already taken. Please try again.")
+    slots = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+
+    while win == False:
+
+        if turn == 'us1':
+            printTable(slots)
+            printTableNums()
+
+            while True:
+                try:
+                    us1ch = int(input(f"What is your choice {usr1n}? Use numbers 1 to 9 to take your pick. Numbers are shown on the second table: "))
+                    break
+
+                except ValueError:
+                    print("This isn't a number, please try again")
+                    errors += 1
+
+
+            if us1ch >= 10:
+                print("Sorry, that's out of the range of the table, please try again.")
                 i = 1
                 errors += 1
-
-
-        if i != 1:
-            turn = turnflip(turn)
-
-
-        win = test(slots, win)
-
-    elif turn == 'us2':
-        i = 0
-        printTable(slots)
-        printTableNums()
-        
-        while True:
-            try:
-                us2ch = int(input(f"What is your choice {usr2n}? Use numbers 1 to 9 to take your pick. Numbers are shown on the second table: "))
-                break
             
-            except ValueError:
-                print("This isn't a number, please try again.")
-
-
-        if us2ch >= 10:
-            print("Sorry, that's out of the range of the table, please try again.")
-            i = 1
-            errors += 1
-        
-        else:
-            if slots[us2ch - 1] == '-':
-                slots[us2ch - 1] = usr2Cch
-                i = 0
-                
-            else:
-                print("Sorry, that tile is already taken. Please try again.")
+            elif us1ch < 1:
+                print("Sorry, that's out of the range of the table, please try again.")
                 i = 1
                 errors += 1
 
 
-        if i != 1:
-            turn = turnflip(turn)
+            else:
+                if slots[us1ch - 1] == '-':
+                    slots[us1ch - 1] = usr1Cch
+                    i = 0
+
+                else:
+                    print("Sorry, that tile is already taken. Please try again.")
+                    i = 1
+                    errors += 1
 
 
-        win = test(slots, win)
+            if i != 1:
+                turn = turnflip(turn)
+
+
+            win = test(slots, win)
+
+        elif turn == 'us2':
+            i = 0
+            printTable(slots)
+            printTableNums()
+            
+            while True:
+                try:
+                    us2ch = int(input(f"What is your choice {usr2n}? Use numbers 1 to 9 to take your pick. Numbers are shown on the second table: "))
+                    break
+                
+                except ValueError:
+                    print("This isn't a number, please try again.")
+                    errors += 1
+
+
+            if us2ch >= 10:
+                print("Sorry, that's out of the range of the table, please try again.")
+                i = 1
+                errors += 1
+
+            elif us1ch < 1:
+                print("Sorry, that's out of the range of the table, please try again.")
+                i = 1
+                errors += 1
+            
+
+            else:
+                if slots[us2ch - 1] == '-':
+                    slots[us2ch - 1] = usr2Cch
+                    i = 0
+                    
+                else:
+                    print("Sorry, that tile is already taken. Please try again.")
+                    i = 1
+                    errors += 1
+
+
+            if i != 1:
+                turn = turnflip(turn)
+
+
+            win = test(slots, win)
 
 
 printTable(slots)
